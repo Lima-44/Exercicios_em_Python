8c07f13f4c7501c3999d328bd15583771a57aa85
Lima-44 <leandrodesantos@gmail.com>
2025-03-17 21:27:04 -0300

    Atualizando e treinando os códigos

--- ex031.py
+++ ex031.py
@@ -1,11 +1,12 @@
 # Desenvolva um programa que pergunte a distância de uma viagem em Km.
 # Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 para viagens mais longas.
 
-dis = int(input('Qual é a distância da viagem em Km: '))
+dis = float(input('Qual é a distância da viagem em Km: '))
 ate = dis <= 200 
 aci = dis > 200
 if dis <= 200:
-    print('O preço da sua passagem ficará em R$ {}'.format((.50 / ate)* dis))
+    print('O preço da sua passagem ficará em R$ {:.2f}'.format((.50 / ate)* dis))
 else:
-    print('Para viagens acima de 200 km, neste caso o preço da passagem é de: R$ {} reais '.format((.45 / aci)* dis))
+    print('Para viagens acima de 200 km, neste caso o preço da passagem é de: R$ {:.2f} reais '.format((.45 / aci)* dis))
 
+#preco = dis * 0.50 if dis <= 200 else dis *0.45 -----> Este é o inline, um comando simplificado.
--- ex034.py
+++ ex034.py
@@ -2,8 +2,17 @@
 # Para salários superiores a R$1.250,00 calcule um aumento de 10%.
 # Para salários inferiores ou iguais, o aumento é de 15%.  
 
-sal = int(input('Digite o seu salário: '))
+sal = float(input('Digite o seu salário: '))
 if sal > 1250:
-    print('O seu novo salário com o aumento de dez porcento R$ {} reais'.format(sal * .10 + sal))
+    print('O seu novo salário com o aumento de dez porcento R$ {:.2f} reais'.format(sal * .10 + sal))
 else:
-    print('O seu novo salário com o aumento de quinze porcento R$ {} reais'.format(sal * .15 + sal))
+    print('O seu novo salário com o aumento de quinze porcento R$ {:.2f} reais'.format(sal * .15 + sal))
+
+    # Fazendo o código de uma outra maneira
+
+salario = float(input('Qual é o salário do funcionário? R$: '))
+if salario <= 1250:
+    novo = salario + (salario * 15 /100)
+else: 
+    novo = salario + (salario * 10 /100)
+    print('Quem ganhava R$ {:.2F} passa a ganhar R$ {:.2f} agora.'.format(salario, novo))
